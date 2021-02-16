d294a676f821db4d3618c322e8729f46beefaa17
Anand Salil <52151136+glitch-anand@users.noreply.github.com>
2021-02-16 10:54:17 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,44 @@
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+# Replace below path with the absolute path of the \
+#chromedriver in your computer
+#WebDriver driver = new FirefoxDriver();
+driver = webdriver.Firefox(executable_path ='/root/Desktop/geckodriver')
+#x-special/nautilus-clipboard
+#copy
+#file:///root/Desktop/geckodriver
+
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600) 
+print("confirmed1")
+  
+# Replace 'Friend's Name' with the name of your friend  
+# or the name of a group  
+target = '"Ann WhatsApp"'
+print("confirmed2")
+  
+# Replace the below string with your own message 
+string = "i love you"
+print("confirmed3")
+  
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located(( By.XPATH, x_arg))) 
+group_title.click() 
+print("confirmed4")
+inp_xpath = '//div[@class="_3FRCZ copyable-text selectable-text"][@dir="ltr"][@data-tab="1"]'
+print("confirmed5")
+input_box = wait.until(EC.presence_of_element_located(( 
+    By.XPATH, inp_xpath))) 
+print("confirmed6")
+for i in range(100): 
+    input_box.send_keys(string + Keys.ENTER) 
+    time.sleep(1) 
+
+
+
